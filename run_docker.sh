c16c7e529335251ded9d8452f675e6bba7206a25
DennisAdenHollander <d.a.d.hollander@student.tue.nl>
2026-01-27 17:57:20 +0100

    test

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,7 @@ xhost +local:root
 
 docker run --rm -it \
   --gpus all \
+  -v "$PWD/roman:/workspace/roman" \
   -v "$PWD/test_data:/workspace/roman/test_data" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
