69b7258fd819a6622e1d9349f578c1cc5529dfea
DennisAdenHollander <d.a.d.hollander@student.tue.nl>
2026-01-27 21:43:38 +0100

    test fix

--- roman/demo/class_count.py
+++ roman/demo/class_count.py
@@ -132,4 +132,173 @@ if __name__ == '__main__':
                 viz_params=mapping_viz_params,
                 verbose=True
             )
+    if not args.skip_align:
+        # TODO: support ground truth pose file for validation
+            
+        for i in range(len(data_params.runs)):
+            if args.skip_indices and i in args.skip_indices:
+                continue
+            for j in range(i, len(data_params.runs)):
+                if args.skip_self_lc and i == j:
+                    continue
+                if args.skip_indices and j in args.skip_indices:
+                    continue
+                output_dir = os.path.join(args.output_dir, "align", f"{data_params.runs[i]}_{data_params.runs[j]}")
+                os.makedirs(output_dir, exist_ok=True)
+                input_files = [os.path.join(args.output_dir, "map", f"{data_params.runs[i]}.pkl"),
+                            os.path.join(args.output_dir, "map", f"{data_params.runs[j]}.pkl")]
+                sm_io = SubmapAlignInputOutput(
+                    inputs=input_files,
+                    output_dir=output_dir,
+                    run_name="align",
+                    lc_association_thresh=args.num_req_assoc,
+                    input_gt_pose_yaml=[gt_files[i], gt_files[j]],
+                    robot_names=[data_params.runs[i], data_params.runs[j]],
+                    robot_env=data_params.run_env,
+                    skip_distance=args.skip_distance
+                )
+                submap_align_params.single_robot_lc = (i == j)
+                submap_align(sm_params=submap_align_params, sm_io=sm_io)
+                       
+    if not args.skip_rpgo:
+        min_keyframe_dist = 0.01 if not offline_rpgo_params.sparsified else 2.0
+        # Create g2o files for odometry
+        for i, run in enumerate(data_params.runs):
+            roman_map_pkl_to_g2o(
+                pkl_file=os.path.join(args.output_dir, "map", f"{run}.pkl"),
+                g2o_file=os.path.join(args.output_dir, "offline_rpgo/sparse", f"{run}.g2o"),
+                time_file=os.path.join(args.output_dir, "offline_rpgo/sparse", f"{run}.time.txt"),
+                robot_id=i,
+                min_keyframe_dist=min_keyframe_dist,
+                t_std=offline_rpgo_params.odom_t_std,
+                r_std=offline_rpgo_params.odom_r_std,
+                verbose=True
+            )
+            
+            # create dense g2o file
+            roman_map_pkl_to_g2o(
+                pkl_file=os.path.join(args.output_dir, "map", f"{run}.pkl"),
+                g2o_file=os.path.join(args.output_dir, "offline_rpgo/dense", f"{run}.g2o"),
+                time_file=os.path.join(args.output_dir, "offline_rpgo/dense", f"{run}.time.txt"),
+                robot_id=i,
+                min_keyframe_dist=None,
+                t_std=offline_rpgo_params.odom_t_std,
+                r_std=offline_rpgo_params.odom_r_std,
+                verbose=True
+            )
+        
+        # Combine timing files
+        odom_sparse_all_time_file = os.path.join(args.output_dir, "offline_rpgo/sparse", "odom_all.time.txt")
+        odom_dense_all_time_file = os.path.join(args.output_dir, "offline_rpgo/dense", "odom_all.time.txt")
+        with open(odom_sparse_all_time_file, 'w') as f:
+            for i, run in enumerate(data_params.runs):
+                with open(os.path.join(args.output_dir, "offline_rpgo/sparse", f"{run}.time.txt"), 'r') as f2:
+                    f.write(f2.read())
+                f2.close()
+        with open(odom_dense_all_time_file, 'w') as f:
+            for i, run in enumerate(data_params.runs):
+                with open(os.path.join(args.output_dir, "offline_rpgo/dense", f"{run}.time.txt"), 'r') as f2:
+                    f.write(f2.read())
+                f2.close()
+        
+        # Fuse all odometry g2o files
+        odom_sparse_all_g2o_file = os.path.join(args.output_dir, "offline_rpgo/sparse", "odom_all.g2o")
+        g2o_fusion_config = create_config(robots=data_params.runs, 
+            odometry_g2o_dir=os.path.join(args.output_dir, "offline_rpgo/sparse"))
+        g2o_file_fusion(g2o_fusion_config, odom_sparse_all_g2o_file, thresh=args.num_req_assoc)
+        
+        # Fuse dense g2o file including loop closures
+        dense_g2o_file = os.path.join(args.output_dir, "offline_rpgo/dense", "odom_and_lc.g2o")
+        g2o_fusion_config = create_config(robots=data_params.runs, 
+            odometry_g2o_dir=os.path.join(args.output_dir, "offline_rpgo/dense"),
+            submap_align_dir=os.path.join(args.output_dir, "align"), align_file_name="align")
+        g2o_file_fusion(g2o_fusion_config, dense_g2o_file, thresh=args.num_req_assoc)
+
+        # Add loop closures to odometry g2o files
+        if offline_rpgo_params.sparsified:
+            final_g2o_file = os.path.join(args.output_dir, "offline_rpgo", "odom_and_lc.g2o")
+            combine_loop_closures(
+                g2o_reference=odom_sparse_all_g2o_file, 
+                g2o_extra_lc=dense_g2o_file, 
+                vertex_times_reference=odom_sparse_all_time_file,
+                vertex_times_extra_lc=odom_dense_all_time_file,
+                output_file=final_g2o_file,
+            )
+        else:
+            final_g2o_file = dense_g2o_file
+        
+        # change lc covar
+        with open(os.path.expanduser(final_g2o_file), 'r') as f:
+            g2o_lines = f.readlines()
+        g2o_lines = edit_g2o_edge_information(g2o_lines, offline_rpgo_params.lc_t_std, 
+                                              offline_rpgo_params.lc_r_std, loop_closures=True)
+        with open(os.path.expanduser(final_g2o_file), 'w') as f:
+            for line in g2o_lines:
+                f.write(line + '\n')
+            f.close()
+            
+        # run kimera centralized robust pose graph optimization
+        result_g2o_file = os.path.join(args.output_dir, "offline_rpgo", "result.g2o")
+        ros_launch_command = f"roslaunch kimera_centralized_pgmo offline_g2o_solver.launch \
+            g2o_file:={final_g2o_file} \
+            output_path:={os.path.join(args.output_dir, 'offline_rpgo')}"
+        roman_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        rpgo_read_g2o_executable = \
+            f"{roman_path}/dependencies/Kimera-RPGO/build/RpgoReadG2o"
+        rpgo_command = f"{rpgo_read_g2o_executable} 3d {final_g2o_file}" \
+            + f" -1.0 -1.0 0.9 {os.path.join(args.output_dir, 'offline_rpgo')} v"
+        os.system(rpgo_command)
+        # os.system(ros_launch_command)
+        
+        # plot results
+        g2o_symbol_to_name = {chr(97 + i): data_params.runs[i] for i in range(len(data_params.runs))}
+        g2o_plot_params = G2OPlotParams()
+        fig, ax = plt.subplots(2, 2, figsize=(10, 10), gridspec_kw={'height_ratios': [5, 1], 'width_ratios': [5, 1]})
+        for i in range(3):
+            g2o_plot_params.axes = [(0, 1), (0, 2), (2, 1)][i]
+            ax_idx = [(0,0), (1,0), (0,1)][i]
+            g2o_plot_params.legend = (i == 0)
+            plot_g2o(
+                g2o_path=result_g2o_file,
+                g2o_symbol_to_name=g2o_symbol_to_name,
+                g2o_symbol_to_color=DEFAULT_TRAJECTORY_COLORS,
+                ax=ax[ax_idx],
+                params=g2o_plot_params
+            )
+
+        # set labels and save figure
+        for i in range(2):
+            ax[i,0].set_xlabel('x (m)')
+            ax[0,i].set_ylabel('y (m)')
+        ax[1,0].set_ylabel('z (m)')
+        ax[0,1].set_xlabel('z (m)')
+        ax[1,1].axis('off')
+        plt.savefig(os.path.join(args.output_dir, "offline_rpgo", "result.png"))
+        print(f"Results saved to {os.path.join(args.output_dir, 'offline_rpgo', 'result.png')}")
+        
+        # Save csv files with resulting trajectories
+        for i, run in enumerate(data_params.runs):
+            pose_data = g2o_and_time_to_pose_data(result_g2o_file, 
+                                                  odom_sparse_all_time_file if offline_rpgo_params.sparsified else odom_dense_all_time_file, 
+                                                  robot_id=i)
+            pose_data.to_csv(os.path.join(args.output_dir, "offline_rpgo", f"{run}.csv"))
+            print(f"Saving {run} pose data to {os.path.join(args.output_dir, 'offline_rpgo', f'{run}.csv')}")
+
+        # Report ATE results
+        if has_gt:
+            ate_rmse = evaluate(
+                result_g2o_file, 
+                odom_sparse_all_time_file  if offline_rpgo_params.sparsified else odom_dense_all_time_file, 
+                {i: gt_files[i] for i in range(len(gt_files))},
+                {i: data_params.runs[i] for i in range(len(data_params.runs))},
+                data_params.run_env,
+                output_dir=args.output_dir
+            )
+            print("ATE results:")
+            print("============")
+            print(ate_rmse)
+            with open(os.path.join(args.output_dir, "offline_rpgo", "ate_rmse.txt"), 'w') as f:
+                print(ate_rmse, file=f)
+                f.close()
+                        
             
